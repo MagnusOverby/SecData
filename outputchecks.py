5719e2ff0a25a9970e97f8ccacace7c5f5633738
Magnus Överby <lambert.overby@gmail.com>
2025-07-08 11:55:37 +0200

    Added Two Checker files

---
+++ outputchecks.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  8 10:19:51 2025
+
+@author: magnu
+"""
+
+"""Wrapper module calling sanity checks implemented in outputchecks_generated.py"""
+
+from outputchecks_generated import run_sanity_checks
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Run sanity checks on statement csvs")
+    parser.add_argument("data_dir", help="Directory containing statement csv files")
+    args = parser.parse_args()
+
+    run_sanity_checks(args.data_dir)
